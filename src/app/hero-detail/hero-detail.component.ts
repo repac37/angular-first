df5e95de8ffcfe275de40e58e2cc889f50fd6094
mickey <mikael.tofvesson@gmail.com>
2022-01-17 15:01:32 +0100

    Added services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit , Input , Output, EventEmitter} from '@angular/core';
 import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,15 +11,18 @@ export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
   @Output() valueChange = new EventEmitter();
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
   addFavorite() { // You can give any function name
-      if(this.hero?.favorit)
+      if(this.hero?.favorit){
        this.valueChange.emit(false);
-      else
-       this.valueChange.emit(true);      
+       this.messageService.add(`HeroDetailComponent: Remove favorit on hero id=${this.hero?.id}`);
+      }else{
+       this.valueChange.emit(true);
+       this.messageService.add(`HeroDetailComponent: Added to favorit on hero id=${this.hero?.id}`);
+      }      
   }
 }
